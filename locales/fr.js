4c1eaf54d500bdde36fbc0ef7c23e6274ebd4491
cmal <cmal@unixcorn.org>
2016-09-07 12:25:37 +0200

    Escaping single quote

--- locales/fr.js
+++ locales/fr.js
@@ -1,5 +1,5 @@
 var locale = [
-  ['à l'instant', 'dans un instant'],
+  ['à l\'instant', 'dans un instant'],
   ['il y a %s secondes', 'dans %s secondes'],
   ['il y a 1 minute', 'dans 1 minute'],
   ['il y a %s minutes', 'dans %s minutes'],
