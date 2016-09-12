9dac17687c549b6a987762cd7514ae3876aea350
Alexey Lizurchik <al.lizurchik@gmail.com>
2016-09-13 01:06:34 +0300

    reduce amount of functions apply currying

--- locales/ru.js
+++ locales/ru.js
@@ -1,36 +1,30 @@
-var seconds = formatAndPrint('секунду', '%s секунду', '%s секунды', '%s секунд'),
-  minutes = formatAndPrint('минуту', '%s минуту', '%s минуты', '%s минут'),
-  hours = formatAndPrint('час', '%s час', '%s часа', '%s часов'),
-  days = formatAndPrint('день', '%s день', '%s дня', '%s дней'),
-  weeks = formatAndPrint('неделю', '%s неделю', '%s недели', '%s недель'),
-  months = formatAndPrint('месяц', '%s месяц', '%s месяца', '%s месяцев'),
-  years = formatAndPrint('год', '%s год', '%s года', '%s лет');
+var seconds = formatNum.bind(null, 'секунду', '%s секунду', '%s секунды', '%s секунд'),
+  minutes = formatNum.bind(null, 'минуту', '%s минуту', '%s минуты', '%s минут'),
+  hours = formatNum.bind(null, 'час', '%s час', '%s часа', '%s часов'),
+  days = formatNum.bind(null, 'день', '%s день', '%s дня', '%s дней'),
+  weeks = formatNum.bind(null, 'неделю', '%s неделю', '%s недели', '%s недель'),
+  months = formatNum.bind(null, 'месяц', '%s месяц', '%s месяца', '%s месяцев'),
+  years = formatNum.bind(null, 'год', '%s год', '%s года', '%s лет');
 
 module.exports = function(number, index) {
   switch(index) {
     case 0: return ['только что', 'через несколько секунд'];
-    case 1: return [seconds(number, ' назад'), seconds(number, 'через ', true)];
+    case 1: return [seconds(number) + ' назад', 'через ' + seconds(number)];
     case 2: // ['минуту назад', 'через минуту'];
-    case 3: return [minutes(number, ' назад'), minutes(number, 'через ', true)];
+    case 3: return [minutes(number) + ' назад', 'через ' + minutes(number)];
     case 4: // ['час назад', 'через час'];
-    case 5: return [hours(number, ' назад'), hours(number, 'через ', true)];
+    case 5: return [hours(number) + ' назад', 'через ' + hours(number)];
     case 6: return ['вчера', 'завтра'];
-    case 7: return [days(number, ' назад'), days(number, 'через ', true)];
+    case 7: return [days(number) + ' назад', 'через ' + days(number)];
     case 8: // ['неделю назад', 'через неделю'];
-    case 9: return [weeks(number, ' назад'), weeks(number, 'через ', true)];
+    case 9: return [weeks(number) + ' назад', 'через ' + weeks(number)];
     case 10: // ['месяц назад', 'через месяц'];
-    case 11: return [months(number, ' назад'), months(number, 'через ', true)];
+    case 11: return [months(number) + ' назад', 'через ' + months(number)];
     case 12: // ['год назад', 'через год'];
-    case 13: return [years(number, ' назад'), years(number, 'через ', true)];
+    case 13: return [years(number) + ' назад', 'через ' + years(number)];
     default: return ['', '']
   }
 };
-
-function formatAndPrint(f1, f, s, t) {
-  return function(n, addition, isBefore) {
-    return addStr(formatNum(f1, f, s, t, n), addition, isBefore);
-  }
-}
 
 /**
  *
@@ -54,10 +48,4 @@ function formatNum(f1, f, s, t, n) {
   }
   
   return str;
-}
-
-function addStr(source, addition, isBefore) {
-  addition = addition || '';
-
-  return isBefore ? (addition + source) : (source + addition);
 }
