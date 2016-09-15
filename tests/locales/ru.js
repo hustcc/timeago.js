2290a0c03c90dfb6302b348ca64f24f120637253
Alexey Lizurchik <al.lizurchik@gmail.com>
2016-09-15 08:07:09 +0300

    add test builder class for easier testing (#43)

--- tests/locales/ru.js
+++ tests/locales/ru.js
@@ -1,68 +1,65 @@
-var timeago = require('../../');
+var tb = require('../test-builder')(Date.now()).register('ru', require('../../locales/ru'), true);
 
 module.exports = function(t) {
-  var tm = timeago('2016-06-20 12:30:00', 'ru');
-  tm.register('ru', require('../../locales/ru'));
+  // test seconds
+  t.equal(tb.subSeconds(1), 'только что');
+  t.equal(tb.subSeconds(10), '10 секунд назад');
+  t.equal(tb.subSeconds(21), '21 секунду назад');
+  t.equal(tb.subSeconds(22), '22 секунды назад');
+  t.equal(tb.addSeconds(1), 'через несколько секунд');
+  t.equal(tb.addSeconds(10), 'через 10 секунд');
+  t.equal(tb.addSeconds(21), 'через 21 секунду');
+  t.equal(tb.addSeconds(22), 'через 22 секунды');
 
-  // test second
-  t.equal(tm.format('2016-06-20 12:30:00'), 'только что');
-  t.equal(tm.format('2016-06-20 12:29:50'), '10 секунд назад');
-  t.equal(tm.format('2016-06-20 12:29:39'), '21 секунду назад');
-  t.equal(tm.format('2016-06-20 12:29:38'), '22 секунды назад');
-  t.equal(tm.format('2016-06-20 12:30:01'), 'через несколько секунд');
-  t.equal(tm.format('2016-06-20 12:30:10'), 'через 10 секунд');
-  t.equal(tm.format('2016-06-20 12:30:21'), 'через 21 секунду');
-  t.equal(tm.format('2016-06-20 12:30:22'), 'через 22 секунды');
+  // test minutes
+  t.equal(tb.subMinutes(1), 'минуту назад');
+  t.equal(tb.subMinutes(2), '2 минуты назад');
+  t.equal(tb.subMinutes(5), '5 минут назад');
+  t.equal(tb.subMinutes(21), '21 минуту назад');
+  t.equal(tb.addMinutes(1), 'через минуту');
+  t.equal(tb.addMinutes(2), 'через 2 минуты');
+  t.equal(tb.addMinutes(5), 'через 5 минут');
+  t.equal(tb.addMinutes(21), 'через 21 минуту');
 
-  // test minute
-  t.equal(tm.format('2016-06-20 12:29:00'), 'минуту назад');
-  t.equal(tm.format('2016-06-20 12:28:00'), '2 минуты назад');
-  t.equal(tm.format('2016-06-20 12:25:00'), '5 минут назад');
-  t.equal(tm.format('2016-06-20 12:9:00'), '21 минуту назад');
-  t.equal(tm.format('2016-06-20 12:31:00'), 'через минуту');
-  t.equal(tm.format('2016-06-20 12:32:00'), 'через 2 минуты');
-  t.equal(tm.format('2016-06-20 12:35:00'), 'через 5 минут');
-  t.equal(tm.format('2016-06-20 12:51:00'), 'через 21 минуту');
+  // test hours
+  t.equal(tb.subHours(1), 'час назад');
+  t.equal(tb.subHours(2), '2 часа назад');
+  t.equal(tb.subHours(5), '5 часов назад');
+  t.equal(tb.subHours(21), '21 час назад');
+  t.equal(tb.addHours(1), 'через час');
+  t.equal(tb.addHours(2), 'через 2 часа');
+  t.equal(tb.addHours(5), 'через 5 часов');
+  t.equal(tb.addHours(21), 'через 21 час');
 
-  // test hour
-  t.equal(tm.format('2016-06-20 11:30:00'), 'час назад');
-  t.equal(tm.format('2016-06-20 10:30:00'), '2 часа назад');
-  t.equal(tm.format('2016-06-20 7:30:00'), '5 часов назад');
-  t.equal(tm.format('2016-06-19 15:30:00'), '21 час назад');
-  t.equal(tm.format('2016-06-20 13:30:00'), 'через час');
-  t.equal(tm.format('2016-06-20 14:30:00'), 'через 2 часа');
-  t.equal(tm.format('2016-06-20 17:30:00'), 'через 5 часов');
-  t.equal(tm.format('2016-06-21 9:30:00'), 'через 21 час');
+  // test days
+  t.equal(tb.subDays(1), 'вчера');
+  t.equal(tb.subDays(2), '2 дня назад');
+  t.equal(tb.subDays(5), '5 дней назад');
+  t.equal(tb.addDays(1), 'завтра');
+  t.equal(tb.addDays(2), 'через 2 дня');
+  t.equal(tb.addDays(5), 'через 5 дней');
 
-  // test day
-  t.equal(tm.format('2016-06-19 12:30:00'), 'вчера');
-  t.equal(tm.format('2016-06-18 12:30:00'), '2 дня назад');
-  t.equal(tm.format('2016-06-15 12:30:00'), '5 дней назад');
-  t.equal(tm.format('2016-06-21 12:30:00'), 'завтра');
-  t.equal(tm.format('2016-06-22 12:30:00'), 'через 2 дня');
-  t.equal(tm.format('2016-06-25 12:30:00'), 'через 5 дней');
+  // test weeks
+  t.equal(tb.subWeeks(1), 'неделю назад');
+  t.equal(tb.subWeeks(2), '2 недели назад');
+  t.equal(tb.addWeeks(1), 'через неделю');
+  t.equal(tb.addWeeks(2), 'через 2 недели');
 
-  // test week
-  t.equal(tm.format('2016-06-13 12:30:00'), 'неделю назад');
-  t.equal(tm.format('2016-06-06 12:30:00'), '2 недели назад');
-  t.equal(tm.format('2016-06-27 12:30:00'), 'через неделю');
-  t.equal(tm.format('2016-07-04 12:30:00'), 'через 2 недели');
+  // test months
+  t.equal(tb.subMonths(1), 'месяц назад');
+  t.equal(tb.subMonths(2), '2 месяца назад');
+  t.equal(tb.subMonths(5), '5 месяцев назад');
+  t.equal(tb.addMonths(1), 'через месяц');
+  t.equal(tb.addMonths(2), 'через 2 месяца');
+  t.equal(tb.addMonths(5), 'через 5 месяцев');
 
-  // test month
-  t.equal(tm.format('2016-05-20 12:30:00'), 'месяц назад');
-  t.equal(tm.format('2016-04-20 12:30:00'), '2 месяца назад');
-  t.equal(tm.format('2016-01-19 12:30:00'), '5 месяцев назад');
-  t.equal(tm.format('2016-07-21 12:30:00'), 'через месяц');
-  t.equal(tm.format('2016-08-20 12:30:00'), 'через 2 месяца');
-  t.equal(tm.format('2016-11-20 12:30:00'), 'через 5 месяцев');
-
-  // test year
-  t.equal(tm.format('2015-06-20 12:30:00'), 'год назад');
-  t.equal(tm.format('2014-06-20 12:30:00'), '2 года назад');
-  t.equal(tm.format('2011-06-20 12:30:00'), '5 лет назад');
-  t.equal(tm.format('1995-06-20 12:30:00'), '21 год назад');
-  t.equal(tm.format('2017-06-20 12:30:00'), 'через год');
-  t.equal(tm.format('2018-06-20 12:30:00'), 'через 2 года');
-  t.equal(tm.format('2021-06-20 12:30:00'), 'через 5 лет');
-  t.equal(tm.format('2037-06-20 12:30:00'), 'через 21 год');
+  // test years
+  t.equal(tb.subYears(1), 'год назад');
+  t.equal(tb.subYears(2), '2 года назад');
+  t.equal(tb.subYears(5), '5 лет назад');
+  t.equal(tb.sub({years: 19, months: 24}), '21 год назад'); // 21 years ago
+  t.equal(tb.addYears(1), 'через год');
+  t.equal(tb.addYears(2), 'через 2 года');
+  t.equal(tb.addYears(5), 'через 5 лет');
+  t.equal(tb.add({years: 20, months: 12}), 'через 21 год'); // in 21 years
 };
