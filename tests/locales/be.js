9da2f11ca89e9081a07555fbf9589032d211e29f
Alexey Lizurchik <al.lizurchik@gmail.com>
2016-09-15 14:45:48 +0300

    update be tests

--- tests/locales/be.js
+++ tests/locales/be.js
@@ -1,68 +1,63 @@
-var timeago = require('../../');
-
-module.exports = function(t) {
-  var tm = timeago('2016-06-20 12:30:00', 'be');
-  tm.register('be', require('../../locales/be'));
-
+module.exports = function(t, tb) {
   // test second
-  t.equal(tm.format('2016-06-20 12:30:00'), 'толькі што');
-  t.equal(tm.format('2016-06-20 12:29:50'), '10 секунд таму');
-  t.equal(tm.format('2016-06-20 12:29:39'), '21 секунду таму');
-  t.equal(tm.format('2016-06-20 12:29:38'), '22 секунды таму');
-  t.equal(tm.format('2016-06-20 12:30:01'), 'праз некалькі секунд');
-  t.equal(tm.format('2016-06-20 12:30:10'), 'праз 10 секунд');
-  t.equal(tm.format('2016-06-20 12:30:21'), 'праз 21 секунду');
-  t.equal(tm.format('2016-06-20 12:30:22'), 'праз 22 секунды');
+  t.equal(tb.subSeconds(1), 'толькі што');
+  t.equal(tb.subSeconds(10), '10 секунд таму');
+  t.equal(tb.subSeconds(21), '21 секунду таму');
+  t.equal(tb.subSeconds(22), '22 секунды таму');
+  t.equal(tb.addSeconds(1), 'праз некалькі секунд');
+  t.equal(tb.addSeconds(10), 'праз 10 секунд');
+  t.equal(tb.addSeconds(21), 'праз 21 секунду');
+  t.equal(tb.addSeconds(22), 'праз 22 секунды');
 
   // test minute
-  t.equal(tm.format('2016-06-20 12:29:00'), 'хвіліну таму');
-  t.equal(tm.format('2016-06-20 12:28:00'), '2 хвіліны таму');
-  t.equal(tm.format('2016-06-20 12:25:00'), '5 хвілін таму');
-  t.equal(tm.format('2016-06-20 12:9:00'), '21 хвіліну таму');
-  t.equal(tm.format('2016-06-20 12:31:00'), 'праз хвіліну');
-  t.equal(tm.format('2016-06-20 12:32:00'), 'праз 2 хвіліны');
-  t.equal(tm.format('2016-06-20 12:35:00'), 'праз 5 хвілін');
-  t.equal(tm.format('2016-06-20 12:51:00'), 'праз 21 хвіліну');
-  
+  t.equal(tb.subMinutes(1), 'хвіліну таму');
+  t.equal(tb.subMinutes(2), '2 хвіліны таму');
+  t.equal(tb.subMinutes(5), '5 хвілін таму');
+  t.equal(tb.subMinutes(21), '21 хвіліну таму');
+  t.equal(tb.addMinutes(1), 'праз хвіліну');
+  t.equal(tb.addMinutes(2), 'праз 2 хвіліны');
+  t.equal(tb.addMinutes(5), 'праз 5 хвілін');
+  t.equal(tb.addMinutes(21), 'праз 21 хвіліну');
+
   // test hour
-  t.equal(tm.format('2016-06-20 11:30:00'), 'гадзіну таму');
-  t.equal(tm.format('2016-06-20 10:30:00'), '2 гадзіны таму');
-  t.equal(tm.format('2016-06-20 7:30:00'), '5 гадзін таму');
-  t.equal(tm.format('2016-06-19 15:30:00'), '21 гадзіну таму');
-  t.equal(tm.format('2016-06-20 13:30:00'), 'праз гадзіну');
-  t.equal(tm.format('2016-06-20 14:30:00'), 'праз 2 гадзіны');
-  t.equal(tm.format('2016-06-20 17:30:00'), 'праз 5 гадзін');
-  t.equal(tm.format('2016-06-21 9:30:00'), 'праз 21 гадзіну');
+  t.equal(tb.subHours(1), 'гадзіну таму');
+  t.equal(tb.subHours(2), '2 гадзіны таму');
+  t.equal(tb.subHours(5), '5 гадзін таму');
+  t.equal(tb.subHours(21), '21 гадзіну таму');
+  t.equal(tb.addHours(1), 'праз гадзіну');
+  t.equal(tb.addHours(2), 'праз 2 гадзіны');
+  t.equal(tb.addHours(5), 'праз 5 гадзін');
+  t.equal(tb.addHours(21), 'праз 21 гадзіну');
 
   // test day
-  t.equal(tm.format('2016-06-19 12:30:00'), 'дзень таму');
-  t.equal(tm.format('2016-06-18 12:30:00'), '2 дні таму');
-  t.equal(tm.format('2016-06-15 12:30:00'), '5 дзён таму');
-  t.equal(tm.format('2016-06-21 12:30:00'), 'праз дзень');
-  t.equal(tm.format('2016-06-22 12:30:00'), 'праз 2 дні');
-  t.equal(tm.format('2016-06-25 12:30:00'), 'праз 5 дзён');
+  t.equal(tb.subDays(1), 'дзень таму');
+  t.equal(tb.subDays(2), '2 дні таму');
+  t.equal(tb.subDays(5), '5 дзён таму');
+  t.equal(tb.addDays(1), 'праз дзень');
+  t.equal(tb.addDays(2), 'праз 2 дні');
+  t.equal(tb.addDays(5), 'праз 5 дзён');
 
   // test week
-  t.equal(tm.format('2016-06-13 12:30:00'), 'тыдзень таму');
-  t.equal(tm.format('2016-06-06 12:30:00'), '2 тыдні таму');
-  t.equal(tm.format('2016-06-27 12:30:00'), 'праз тыдзень');
-  t.equal(tm.format('2016-07-04 12:30:00'), 'праз 2 тыдні');
+  t.equal(tb.subWeeks(1), 'тыдзень таму');
+  t.equal(tb.subWeeks(2), '2 тыдні таму');
+  t.equal(tb.addWeeks(1), 'праз тыдзень');
+  t.equal(tb.addWeeks(2), 'праз 2 тыдні');
 
   // test month
-  t.equal(tm.format('2016-05-20 12:30:00'), 'месяц таму');
-  t.equal(tm.format('2016-04-20 12:30:00'), '2 месяцы таму');
-  t.equal(tm.format('2016-01-19 12:30:00'), '5 месяцаў таму');
-  t.equal(tm.format('2016-07-21 12:30:00'), 'праз месяц');
-  t.equal(tm.format('2016-08-20 12:30:00'), 'праз 2 месяцы');
-  t.equal(tm.format('2016-11-20 12:30:00'), 'праз 5 месяцаў');
+  t.equal(tb.subMonths(1), 'месяц таму');
+  t.equal(tb.subMonths(2), '2 месяцы таму');
+  t.equal(tb.subMonths(5), '5 месяцаў таму');
+  t.equal(tb.addMonths(1), 'праз месяц');
+  t.equal(tb.addMonths(2), 'праз 2 месяцы');
+  t.equal(tb.addMonths(5), 'праз 5 месяцаў');
 
   // test year
-  t.equal(tm.format('2015-06-20 12:30:00'), 'год таму');
-  t.equal(tm.format('2014-06-20 12:30:00'), '2 гады таму');
-  t.equal(tm.format('2011-06-20 12:30:00'), '5 гадоў таму');
-  t.equal(tm.format('1995-06-20 12:30:00'), '21 год таму');
-  t.equal(tm.format('2017-06-20 12:30:00'), 'праз год');
-  t.equal(tm.format('2018-06-20 12:30:00'), 'праз 2 гады');
-  t.equal(tm.format('2021-06-20 12:30:00'), 'праз 5 гадоў');
-  t.equal(tm.format('2037-06-20 12:30:00'), 'праз 21 год');
+  t.equal(tb.subYears(1), 'год таму');
+  t.equal(tb.subYears(2), '2 гады таму');
+  t.equal(tb.subYears(5), '5 гадоў таму');
+  t.equal(tb.subYears(21), '21 год таму');
+  t.equal(tb.addYears(1), 'праз год');
+  t.equal(tb.addYears(2), 'праз 2 гады');
+  t.equal(tb.addYears(5), 'праз 5 гадоў');
+  t.equal(tb.addYears(21), 'праз 21 год');
 };
