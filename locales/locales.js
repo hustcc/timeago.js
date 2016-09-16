2b28e5d28f04ed6c0c72e3346cae3def97a01600
Ankur Jyoti Phukan <leocreed1994@gmail.com>
2016-09-16 19:05:29 +0530

    Ajphukan upload 1 (#55)
    * Add files via upload
    
    Hindi translation for locale
    
    * Update locales.js
    
    add ind_hin
    
    * Add files via upload
    
    Locale translation for 
    Country: India
    Language: Bengali
    
    * Rename ind_hin.js to in_hi.js
    
    Changing Keyword from ind_hin.js to in_hi.js
    
    * Update locales.js
    
    Added in_BG and Changed ind_hin to in_HI
    
    * Rename in_hi.js to in_HI.js
    
    in_hi to in_HI
    
    * Update in_HI.js
    
    Deleted some white space
    
    * Update in_HI.js
    
    * Create in_HI.js
    
    * Update in_HI.js
    
    * Update in_HI.js
    
    * Update in_HI.js
    
    * Create in_BG.js
    
    test for in_BG

--- locales/locales.js
+++ locales/locales.js
@@ -3,4 +3,4 @@
  * 1. please sort by dictionary.
  * 2. please ensure the accuracy.
  */
-module.exports = ['ar', 'az', 'az', 'be', 'bg', 'bs', 'ca', 'cs', 'cy', 'da', 'de', 'dv', 'el', 'en', 'en_short', 'es', 'et', 'eu', 'fa', 'fi', 'fr', 'gl', 'he', 'hr', 'hu', 'hy', 'id', 'in_ID', 'is', 'it', 'ja', 'jv', 'ko', 'ky', 'lt', 'lv', 'mk', 'ml' , 'nb_NO', 'nl', 'nn_NO', 'no', 'pl', 'pt', 'pt_BR', 'ro', 'rs', 'ru', 'rw', 'si', 'sk', 'sl', 'sr', 'sv', 'th', 'tr', 'uk', 'uz', 'vi', 'zh_CN', 'zh_TW'];
+module.exports = ['ar', 'az', 'az', 'be', 'bg', 'bs', 'ca', 'cs', 'cy', 'da', 'de', 'dv', 'el', 'en', 'en_short', 'es', 'et', 'eu', 'fa', 'fi', 'fr', 'gl', 'he', 'hr', 'hu', 'hy', 'id', 'in_HI', 'in_BG', 'in_ID', 'is', 'it', 'ja', 'jv', 'ko', 'ky', 'lt', 'lv', 'mk', 'ml' , 'nb_NO', 'nl', 'nn_NO', 'no', 'pl', 'pt', 'pt_BR', 'ro', 'rs', 'ru', 'rw', 'si', 'sk', 'sl', 'sr', 'sv', 'th', 'tr', 'uk', 'uz', 'vi', 'zh_CN', 'zh_TW'];
