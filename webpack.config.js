cde9577fa7a690814436cdcb895fdb223234fe93
hustcc <i@atool.org>
2016-09-20 17:14:33 +0800

    fixed #72 remove unused config

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,9 @@ var uglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
   output: {comments: false},
   compress: {warnings: false}
 });
-var definePlugin = new webpack.DefinePlugin({
-  'process.env': {NODE_ENV: '"production"'},
-});
 
 module.exports = {
   entry: './src/timeago.locales.js',
   output: {filename: './dist/timeago.locales.min.js'},
-  plugins: [uglifyJsPlugin, definePlugin]
+  plugins: [uglifyJsPlugin]
 };
