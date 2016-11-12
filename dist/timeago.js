a9462dd94488cc67155a3bc4a66cd0d74f88e6cd
aTool <i@atool.org>
2016-11-12 21:23:52 +0800

    repack dist files #91

--- dist/timeago.js
+++ dist/timeago.js
@@ -96,6 +96,7 @@ function () {
   }
   // get the datetime attribute, jQuery and DOM
   function getDateAttr(node) {
+    if(node.dataset.timeago) return node.dataset.timeago;
     if (node.getAttribute) return node.getAttribute(ATTR_DATETIME);
     if(node.attr) return node.attr(ATTR_DATETIME);
   }
