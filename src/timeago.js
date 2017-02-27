98e3f6c004c68c9f4abc64688746398216b4e516
Alexey Lizurchik <al.lizurchik@gmail.com>
2017-02-27 13:12:06 +0300

    Add ability to cancel render for specific node, update readme

--- src/timeago.js
+++ src/timeago.js
@@ -32,8 +32,18 @@ function () {
     SEC_ARRAY = [60, 60, 24, 7, 365/7/12, 12],
     SEC_ARRAY_LEN = 6,
     ATTR_DATETIME = 'datetime',
-    timers = []; // real-time render timers
+    timers = {}; // real-time render timers
 
+  function findTimeoutByNode(node) {
+    var result, key;
+    for (key in timers) {
+      if (timers[key] === node) {
+        result = key;
+        break;
+      }
+    }
+    return result;
+  }
   // format Date / string / timestamp to Date instance.
   function toDate(input) {
     if (input instanceof Date) return input;
@@ -141,11 +151,12 @@ function () {
   Timeago.prototype.doRender = function(node, date, locale) {
     var diff = diffSec(date, this.nowDate),
       self = this;
+    delete timers[findTimeoutByNode(node)]; // delete previously assigned timeout's id to node
     node.innerHTML = formatDiff(diff, locale, this.defaultLocale);
     // waiting %s seconds, do the next render
-    timers.push(setTimeout(function() {
+    timers[setTimeout(function() {
       self.doRender(node, date, locale);
-    }, Math.min(nextInterval(diff) * 1000, 0x7FFFFFFF)));
+    }, Math.min(nextInterval(diff) * 1000, 0x7FFFFFFF))] = node;
   };
   /**
    * format: format the date to *** time ago, with setting or default locale
@@ -180,20 +191,6 @@ function () {
     for (var i = 0, len = nodes.length; i < len; i++) {
       this.doRender(nodes[i], getDateAttr(nodes[i]), locale); // render item
     }
-  };
-  /**
-   * cancel: cancel all the timers which are doing real-time render.
-   *
-   * How to use it?
-   * var timeago = new require('timeago.js')();
-   * timeago.render(document.querySelectorAll('.need_to_be_rendered'));
-   * timeago.cancel(); // will stop all the timer, stop render in real time.
-  **/
-  Timeago.prototype.cancel = function() {
-    for (var i = 0, len = timers.length; i < len; i++) {
-      clearTimeout(timers[i]);
-    }
-    timers = [];
   };
   /**
    * setLocale: set the default locale name.
@@ -235,6 +232,37 @@ function () {
    **/
   timeagoFactory.register = function(locale, localeFunc) {
     locales[locale] = localeFunc;
+  };
+
+  /**
+   * cancel: cancels one or all the timers which are doing real-time render.
+   *
+   * How to use it?
+   * For canceling all the timers:
+   * var timeagoFactory = require('timeago.js');
+   * var timeago = timeagoFactory();
+   * timeago.render(document.querySelectorAll('.need_to_be_rendered'));
+   * timeagoFactory.cancel(); // will stop all the timers, stop render in real time.
+   *
+   * For canceling single timer on specific node:
+   * var timeagoFactory = require('timeago.js');
+   * var timeago = timeagoFactory();
+   * var nodes = document.querySelectorAll('.need_to_be_rendered');
+   * timeago.render(nodes);
+   * timeagoFactory.cancel(nodes[0]); // will clear a timer attached to the first node, stop render in real time.
+   **/
+  timeagoFactory.cancel = function(node) {
+    var timeoutId;
+    // assigning in if statement to save space
+    if (node && (timeoutId = findTimeoutByNode(node))) {
+      clearTimeout(timeoutId);
+      delete timers[timeoutId];
+    } else {
+      for (var key in timers) {
+        clearTimeout(key);
+      }
+      timers = {};
+    }
   };
 
   return timeagoFactory;
