a7000eaff95869a8587e9f62344740f593d458f5
Alexey Lizurchik <al.lizurchik@gmail.com>
2017-04-04 15:56:03 +0300

    Support --noImplicitAny option for TypeScript (#126)
    * Support --noImplicitAny option for TypeScript
    
    * Run compiled TS test in separate task

--- timeago.d.ts
+++ timeago.d.ts
@@ -1,17 +1,15 @@
 /// <reference types="jquery" />
 type TDate = Date | string | number;
-export interface TimeAgoConstructor {
-    new ();
-    new (nowDate: TDate, defaultLocale?: string);
-    format(date: TDate, locale?: string): string;
-    render<T>(nodes: Node | NodeList | JQuery, locale?: string): void;
-    setLocale(locale: string): void;
+export interface TimeAgoInterface {
+  format(date: TDate, locale?: string): string;
+  render<T>(nodes: Node | NodeList | JQuery, locale?: string): void;
+  setLocale(locale: string): void;
 }
 export interface TimeagoFactory {
-    (): TimeAgoConstructor;
-    (nowDate: TDate, defaultLocale?: string): TimeAgoConstructor;
-    cancel(node?: Node | JQuery): void;
-    register(locale: string, localeFunc: Function): void;
+  (): TimeAgoInterface;
+  (nowDate: TDate, defaultLocale?: string): TimeAgoInterface;
+  cancel(node?: Node | JQuery): void;
+  register(locale: string, localeFunc: Function): void;
 }
 declare let timeagoFactory: TimeagoFactory;
 export default timeagoFactory;
