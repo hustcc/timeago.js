b642a19b67dbd31c0d1d3e1ebaaf6a7287533c19
hustcc <i@hust.cc>
2018-05-20 12:54:05 +0800

    add testcase for entry

---
+++ __tests__/index.spec.js
@@ -0,0 +1,25 @@
+/**
+ * Created by hustcc on 18/5/20.
+ * Contract: i@hust.cc
+ */
+
+import timeago from '../src';
+// import { TimeAgo } from '../src/TimeAgo';
+
+import timeagoLib from '..';
+
+describe('index', () => {
+  test('timeago', () => {
+    expect(timeago).toBeFunction();
+    expect(timeago.register).toBeFunction();
+    expect(timeago.cancel).toBeFunction();
+
+    // expect(timeago() instanceof TimeAgo).toBe(true);
+  });
+
+  test('timeagoLib', () => {
+    expect(timeagoLib).toBeFunction();
+    expect(timeagoLib.register).toBeFunction();
+    expect(timeagoLib.cancel).toBeFunction();
+  });
+});
