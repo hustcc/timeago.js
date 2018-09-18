223d8e22e27314ec6de6b076d3199e44f63f9656
hustcc <i@hust.cc>
2018-09-18 17:26:01 +0800

    add resolve

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@
 
 import uglify from 'rollup-plugin-uglify';
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
@@ -14,6 +15,7 @@ export default {
     format: 'umd',
   },
   plugins: [
+    resolve(),
     babel({
       exclude: 'node_modules/**',
     }),
