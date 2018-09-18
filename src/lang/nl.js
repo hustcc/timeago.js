1d74094965842d0fccc218dbaf1bebf4d52dabfa
Robin Bron <robin@finwo.nl>
2018-09-18 11:42:19 +0200

    Fixed dutch grammar exclusion (#161)
    * Fixed dutch grammar exclusion
    
    * Updated NL locale in src/ instead of lib/
    
    * Added test for NL locale

--- src/lang/nl.js
+++ src/lang/nl.js
@@ -5,7 +5,7 @@ export default function(number, index) {
     ['1 minuut geleden', 'binnen 1 minuut'],
     ['%s minuten geleden', 'binnen %s minuten'],
     ['1 uur geleden', 'binnen 1 uur'],
-    ['%s uren geleden', 'binnen %s uren'],
+    ['%s uur geleden', 'binnen %s uur'],
     ['1 dag geleden', 'binnen 1 dag'],
     ['%s dagen geleden', 'binnen %s dagen'],
     ['1 week geleden', 'binnen 1 week'],
@@ -13,6 +13,6 @@ export default function(number, index) {
     ['1 maand geleden', 'binnen 1 maand'],
     ['%s maanden geleden', 'binnen %s maanden'],
     ['1 jaar geleden', 'binnen 1 jaar'],
-    ['%s jaren geleden', 'binnen %s jaren']
+    ['%s jaar geleden', 'binnen %s jaar']
   ][index];
 }
