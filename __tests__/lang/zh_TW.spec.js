23ea67212e7882bcbd64e645f668e1e3de5b4b75
hustcc <i@hust.cc>
2018-12-12 13:32:59 +0800

    update zh_TW.spec

--- __tests__/lang/zh_TW.spec.js
+++ __tests__/lang/zh_TW.spec.js
@@ -3,12 +3,11 @@
  * Contact: cubemail88@gmail.com
  */
 import { advanceBy, advanceTo, clear } from 'jest-date-mock';
-import Timeago from '../../src';
+import { register, format } from '../../src';
 import zh_TW from '../../src/lang/zh_TW';
 
-Timeago.register('zh_TW', zh_TW);
+register('zh_TW', zh_TW);
 
-const timeago = Timeago();
 let date = new Date();
 
 beforeEach(() => {
@@ -21,88 +20,88 @@ afterEach(() => {
 describe('zh_TW', () => {
   test('time ago', () => {
     advanceTo(9 * 1000);
-    expect(timeago.format(date, 'zh_TW')).toEqual('剛剛');
+    expect(format(date, 'zh_TW')).toEqual('剛剛');
 
     advanceTo(30 * 1000);
-    expect(timeago.format(date, 'zh_TW')).toEqual('30 秒前');
+    expect(format(date, 'zh_TW')).toEqual('30 秒前');
 
     advanceTo(1000 * 60);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 分鐘前');
+    expect(format(date, 'zh_TW')).toEqual('1 分鐘前');
 
     advanceTo(1000 * 60 * 30);
-    expect(timeago.format(date, 'zh_TW')).toEqual('30 分鐘前');
+    expect(format(date, 'zh_TW')).toEqual('30 分鐘前');
 
     advanceTo(1000 * 60 * 60);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 小時前');
+    expect(format(date, 'zh_TW')).toEqual('1 小時前');
 
     advanceTo(1000 * 60 * 60 * 8);
-    expect(timeago.format(date, 'zh_TW')).toEqual('8 小時前');
+    expect(format(date, 'zh_TW')).toEqual('8 小時前');
 
     advanceTo(1000 * 60 * 60 * 24);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 天前');
+    expect(format(date, 'zh_TW')).toEqual('1 天前');
 
     advanceTo(1000 * 60 * 60 * 24 * 3);
-    expect(timeago.format(date, 'zh_TW')).toEqual('3 天前');
+    expect(format(date, 'zh_TW')).toEqual('3 天前');
 
     advanceTo(1000 * 60 * 60 * 24 * 7);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 週前');
+    expect(format(date, 'zh_TW')).toEqual('1 週前');
 
     advanceTo(1000 * 60 * 60 * 24 * 7 * 3);
-    expect(timeago.format(date, 'zh_TW')).toEqual('3 週前');
+    expect(format(date, 'zh_TW')).toEqual('3 週前');
 
     advanceTo(1000 * 60 * 60 * 24 * 31);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 月前');
+    expect(format(date, 'zh_TW')).toEqual('1 月前');
 
     advanceTo(1000 * 60 * 60 * 24 * 31 * 4);
-    expect(timeago.format(date, 'zh_TW')).toEqual('4 月前');
+    expect(format(date, 'zh_TW')).toEqual('4 月前');
 
     advanceTo(1000 * 60 * 60 * 24 * 366);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 年前');
+    expect(format(date, 'zh_TW')).toEqual('1 年前');
 
     advanceTo(1000 * 60 * 60 * 24 * 366 * 10);
-    expect(timeago.format(date, 'zh_TW')).toEqual('10 年前');
+    expect(format(date, 'zh_TW')).toEqual('10 年前');
   });
   test('time in', () => {
     advanceTo(-9 * 1000);
-    expect(timeago.format(date, 'zh_TW')).toEqual('片刻後');
+    expect(format(date, 'zh_TW')).toEqual('片刻後');
 
     advanceTo(-30 * 1000);
-    expect(timeago.format(date, 'zh_TW')).toEqual('30 秒後');
+    expect(format(date, 'zh_TW')).toEqual('30 秒後');
 
     advanceTo(-1000 * 60);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 分鐘後');
+    expect(format(date, 'zh_TW')).toEqual('1 分鐘後');
 
     advanceTo(-1000 * 60 * 30);
-    expect(timeago.format(date, 'zh_TW')).toEqual('30 分鐘後');
+    expect(format(date, 'zh_TW')).toEqual('30 分鐘後');
 
     advanceTo(-1000 * 60 * 60);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 小時後');
+    expect(format(date, 'zh_TW')).toEqual('1 小時後');
 
     advanceTo(-1000 * 60 * 60 * 8);
-    expect(timeago.format(date, 'zh_TW')).toEqual('8 小時後');
+    expect(format(date, 'zh_TW')).toEqual('8 小時後');
 
     advanceTo(-1000 * 60 * 60 * 24);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 天後');
+    expect(format(date, 'zh_TW')).toEqual('1 天後');
 
     advanceTo(-1000 * 60 * 60 * 24 * 3);
-    expect(timeago.format(date, 'zh_TW')).toEqual('3 天後');
+    expect(format(date, 'zh_TW')).toEqual('3 天後');
 
     advanceTo(-1000 * 60 * 60 * 24 * 7);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 週後');
+    expect(format(date, 'zh_TW')).toEqual('1 週後');
 
     advanceTo(-1000 * 60 * 60 * 24 * 7 * 3);
-    expect(timeago.format(date, 'zh_TW')).toEqual('3 週後');
+    expect(format(date, 'zh_TW')).toEqual('3 週後');
 
     advanceTo(-1000 * 60 * 60 * 24 * 31);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 月後');
+    expect(format(date, 'zh_TW')).toEqual('1 月後');
 
     advanceTo(-1000 * 60 * 60 * 24 * 31 * 4);
-    expect(timeago.format(date, 'zh_TW')).toEqual('4 月後');
+    expect(format(date, 'zh_TW')).toEqual('4 月後');
 
     advanceTo(-1000 * 60 * 60 * 24 * 366);
-    expect(timeago.format(date, 'zh_TW')).toEqual('1 年後');
+    expect(format(date, 'zh_TW')).toEqual('1 年後');
 
     advanceTo(-1000 * 60 * 60 * 24 * 366 * 10);
-    expect(timeago.format(date, 'zh_TW')).toEqual('10 年後');
+    expect(format(date, 'zh_TW')).toEqual('10 年後');
   });
 });
