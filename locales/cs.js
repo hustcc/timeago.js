79e220bb2dfaa683e6d657747e8b00bfe503ab7d
Erik Vaněk <10098945+erikvanek@users.noreply.github.com>
2019-05-29 15:04:34 +0200

    Czech translatio (#184)
    * Czech translation
    
    * fix: remove trailing whitespace
    
    * fix: indent properly

---
+++ locales/cs.js
@@ -0,0 +1,18 @@
+module.exports = function(number, index) {
+  return [
+    ['právě teď', 'právě teď'],
+    ['před %s vteřinami', 'za %s vteřin'],
+    ['před minutou', 'za minutu'],
+    ['před %s minutami', 'za %s minut'],
+    ['před hodinou', 'za hodinu'],
+    ['před %s hodinami', 'za %s hodin'],
+    ['včera', 'zítra'],
+    ['před %s dny', 'za %s dny'],
+    ['minulý týden', 'příští týden'],
+    ['před %s týdny', 'za %s týdnů'],
+    ['minulý měsíc', 'přístí měsíc'],
+    ['před %s měsíci', 'za %s měsíců'],
+    ['vloni', 'přístí rok'],
+    ['před %s lety', 'za %s let']
+  ][index];
+}
