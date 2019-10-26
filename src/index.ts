b24ece58c13d020452c22acbfd24f762b5d8ae79
Alex R <alexei.rudenko@gmail.com>
2019-10-26 08:41:36 +0200

    Simplify built-in locales and move tests

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,9 @@ import zh_CN from './lang/zh_CN';
 
 import { register } from './register';
 
-export function registerBuiltInLocales() {
-  register('en_US', en_US);
-  register('zh_CN', zh_CN);
-}
+register('en_US', en_US);
+register('zh_CN', zh_CN);
 
 export { format } from './format';
 export { render, cancel } from './realtime';
 export { register };
-
-registerBuiltInLocales();
