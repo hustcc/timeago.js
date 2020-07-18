76b3d0ec43427aeae6e8cab361de5cb998f79bd6
Juan Pablo Martínez <jpmart@unizar.es>
2020-07-18 20:09:10 +0200

    Create an.ts

---
+++ src/lang/an.ts
@@ -0,0 +1,18 @@
+export default function(number: number, index: number): [string, string] {
+  return [
+    ['fa un momento', "d'aquí a un momento"],
+    ['fa %s segundos', "d'aquí a %s segundos"],
+    ['fa 1 minut', "d'aquí a 1 minuto"],
+    ['fa %s minutos', "d'aquí a %s minutos"],
+    ['fa 1 hora', "d'aquí a 1 hora"],
+    ['fa %s horas', "d'aquí a %s horas"],
+    ['fa 1 día', "d'aquí a 1 día"],
+    ['fa %s días', "d'aquí a %s días"],
+    ['fa 1 semana', "d'aquí a 1 semana"],
+    ['fa %s semanas', "d'aquí a %s semanas"],
+    ['fa 1 mes', "d'aquí a 1 mes"],
+    ['fa %s meses', "d'aquí a %s meses"],
+    ['fa 1 anyo', "d'aquí a 1 anyo"],
+    ['fa %s anyos', "d'aquí a %s anyos"],
+  ][index] as [string, string];
+}
