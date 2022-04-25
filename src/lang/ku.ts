c9fc8e2841a064ec8d583f9b10f71e751f227ba8
aryankarim <aryanmajid97@gmail.com>
2022-04-25 16:46:56 +0300

    feat(Locales): add Kurdish Locale

---
+++ src/lang/ku.ts
@@ -0,0 +1,20 @@
+const kurdishDigits = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+
+// change digits from English to Kurdish
+function toKurdishNumeric(number: number): string {
+  return number.toString().replace(/\d/g, (x: string) => kurdishDigits[x]);
+}
+
+const kurdishFuzzyDates = [
+  ['%s چرکە لەمەوپێش'], // Seconds
+  ['%s دەقە لەمەوپێش'], // Minutes
+  ['%s کاتژمێر لەمەوپێش'], // Hours
+  ['%s ڕۆژ لەمەوپێش'], // Days
+  ['%s هەفتە لەمەوپێش'], // Weeks
+  ['%s مانگ لەمەوپێش'], // Months
+  ['%s ساڵ لەمەوپێش'], // Years
+];
+
+export default function(number: number, index: number): [string] {
+  return [kurdishFuzzyDates[Math.floor(index / 2)][0].replace('%s', toKurdishNumeric(number))];
+}
